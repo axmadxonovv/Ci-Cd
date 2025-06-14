2c15905ed483b29d5691b65c9d1d216ece3c222f
Avazbek <apple@MacBook-Pro-apple.local>
2025-06-14 17:54:14 +0500

    text changed

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello kimdir Ci !';
+    return 'Hello kimdir Ci!';
   }
 }
